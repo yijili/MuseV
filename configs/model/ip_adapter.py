39d7c357c8fa1528ed30f0139389dce1756f2d3b
yijili <381747118@qq.com>
2025-08-08 19:58:56 +0800

    Update ip_adapter.py

--- configs/model/ip_adapter.py
+++ configs/model/ip_adapter.py
@@ -1,12 +1,14 @@
 import os
 
 IPAdapterModelDir = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), "../../checkpoints", "IP-Adapter"
+    # os.path.dirname(os.path.abspath(__file__)), "../../checkpoints", "IP-Adapter"
+    "/workplace/musev/MuseV/", "checkpoints", "IP-Adapter/"
 )
 
 
 MotionDir = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), "../../checkpoints", "motion"
+    # os.path.dirname(os.path.abspath(__file__)), "../../checkpoints", "motion"
+    "/workplace/musev/MuseV/", "checkpoints", "motion"
 )
 
 
@@ -20,7 +22,7 @@ MODEL_CFG = {
         "desp": "",
     },
     "IPAdapterPlus": {
-        "ip_image_encoder": os.path.join(IPAdapterModelDir, "image_encoder"),
+        "ip_image_encoder": os.path.join(IPAdapterModelDir, "models/image_encoder"),
         "ip_ckpt": os.path.join(IPAdapterModelDir, "ip-adapter-plus_sd15.bin"),
         "ip_scale": 1.0,
         "clip_extra_context_tokens": 16,
@@ -28,7 +30,7 @@ MODEL_CFG = {
         "desp": "",
     },
     "IPAdapterPlus-face": {
-        "ip_image_encoder": os.path.join(IPAdapterModelDir, "image_encoder"),
+        "ip_image_encoder": os.path.join(IPAdapterModelDir, "models/image_encoder"),
         "ip_ckpt": os.path.join(IPAdapterModelDir, "ip-adapter-plus-face_sd15.bin"),
         "ip_scale": 1.0,
         "clip_extra_context_tokens": 16,
@@ -36,7 +38,7 @@ MODEL_CFG = {
         "desp": "",
     },
     "IPAdapterFaceID": {
-        "ip_image_encoder": os.path.join(IPAdapterModelDir, "image_encoder"),
+        "ip_image_encoder": os.path.join(IPAdapterModelDir, "models/image_encoder"),
         "ip_ckpt": os.path.join(IPAdapterModelDir, "ip-adapter-faceid_sd15.bin"),
         "ip_scale": 1.0,
         "clip_extra_context_tokens": 4,
@@ -44,7 +46,7 @@ MODEL_CFG = {
         "desp": "",
     },
     "musev_referencenet": {
-        "ip_image_encoder": os.path.join(IPAdapterModelDir, "image_encoder"),
+        "ip_image_encoder": os.path.join(IPAdapterModelDir, "models/image_encoder"),
         "ip_ckpt": os.path.join(
             MotionDir, "musev_referencenet/ip_adapter_image_proj.bin"
         ),
@@ -54,7 +56,7 @@ MODEL_CFG = {
         "desp": "",
     },
     "musev_referencenet_pose": {
-        "ip_image_encoder": os.path.join(IPAdapterModelDir, "image_encoder"),
+        "ip_image_encoder": os.path.join(IPAdapterModelDir, "models/image_encoder"),
         "ip_ckpt": os.path.join(
             MotionDir, "musev_referencenet_pose/ip_adapter_image_proj.bin"
         ),
