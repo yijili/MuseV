d97a21de20d2a5affcfaf7cf5cabb5ce3d39b983
yijili <381747118@qq.com>
2025-08-19 16:07:41 +0800

    Update app.py

--- scripts/gradio/app.py
+++ scripts/gradio/app.py
@@ -11,7 +11,7 @@ from huggingface_hub import snapshot_download
 
 ProjectDir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 CheckpointsDir = os.path.join(ProjectDir, "checkpoints")
-ignore_video2video = False
+ignore_video2video = True
 max_image_edge = 1280
 
 
